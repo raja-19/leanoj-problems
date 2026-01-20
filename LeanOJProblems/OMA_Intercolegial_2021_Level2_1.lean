7cbe86de5ec06d681810b1eb141c45e4625801d0
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2026-01-20 20:02:19 -0300

    Add 2021 OMA Intercolegial Level 2 problem 1

---
+++ LeanOJProblems/OMA_Intercolegial_2021_Level2_1.lean
@@ -0,0 +1,16 @@
+import Mathlib.Analysis.RCLike.Basic
+
+def sol : ℝ := sorry
+
+noncomputable
+def List.Vector.mean (v : List.Vector ℝ 16) : ℝ :=
+  v.toList.sum / 16
+
+def List.Vector.modify (v : List.Vector ℝ 16) : List.Vector ℝ 16 :=
+  List.Vector.ofFn <| fun i ↦
+    if i < 8 then
+      v.get i - 3
+    else
+      v.get i + 10
+
+lemma valid : ∀ v : List.Vector ℝ 16, v.mean = 168 → (v.modify).mean = sol := sorry
