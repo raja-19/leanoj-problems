6faf2224ec3fb255a62f7e768a0ed957aad366dc
Ansar <azhdarov.ansar@gmail.com>
2026-01-24 18:36:16 +0100

    error in problem statement fixed

--- LeanOJProblems/rational_cube_sum.lean
+++ LeanOJProblems/rational_cube_sum.lean
@@ -1,4 +1,4 @@
 import Mathlib.Data.Rat.Init
 
-theorem solution (n : ℚ) (hn : ∃ x y : ℚ, x ^ 3 + y ^ 3 = n) :
+theorem solution (n : ℚ) (h1 : n ≥ 0) (h2 : ∃ x y, x ^ 3 + y ^ 3 = n) :
     ∃ a b : NNRat, a ^ 3 + b ^ 3 = n := sorry
